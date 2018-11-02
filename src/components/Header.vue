aec6fd3218cb6dd0dfe556e8a830e62b4844745a
Jakub Kaczmarzyk <jakubk@mit.edu>
2018-11-02 12:04:25 -0400

    add header component

---
+++ src/components/Header.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>Nobrainer JS</h1>
+    <p class="text-center">Predict on biomedical images using pre-trained TensorFlow models, all in your browser.</p>
+    <p class="text-center alert-warning">This is experimental and a work in progress.</p>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'Header',
+  props: {},
+  data() {
+    return {};
+  },
+  computed: {},
+  methods: {},
+});
+</script>
+
+<style scoped>
+</style>
